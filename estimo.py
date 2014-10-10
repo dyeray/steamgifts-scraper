01f26f2ecdfb3e629fc451490d0a56a4614b994a
dyeray <dyeray@ull.es>
2014-10-10 13:45:24 +0100

    Added -scan parameters, that scans the frontpage for new games, and asks the user if s/he wants to access giveaways for the new games

--- estimo.py
+++ estimo.py
@@ -1,36 +1,63 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import json
+import sys
+
+SETTINGS_PATH = 'settings.cfg'
 
 
 class Estimo:
     def __init__(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.drv = webdriver.Firefox()
+        self.drv.implicitly_wait(10)
         self.base_url = "http://www.steamgifts.com"
         self.verificationErrors = []
         self.accept_next_alert = True
-        settings = json.load(open('settings.cfg'))
-        self.username = settings['username']
-        self.password = settings['password']
+        self.settings = self._load_settings()
 
     def run(self):
         self.estimo()
         self.close()
 
     def close(self):
-        self.driver.quit()
+        self.drv.quit()
+
+    def login(self):
+        self.drv.get(self.base_url + "/")
+        self.drv.find_element_by_css_selector("img.login").click()
+        self.drv.find_element_by_id("steamAccountName").clear()
+        self.drv.find_element_by_id("steamAccountName").send_keys(self.settings['username'])
+        self.drv.find_element_by_id("steamPassword").clear()
+        self.drv.find_element_by_id("steamPassword").send_keys(self.settings['password'])
+        self.drv.find_element_by_id("imageLogin").click()
+
+    def _scan(self):
+        self.drv.get(self.base_url + "/")
+        return self.drv.find_elements_by_xpath(
+            '//div[@class="ajax_gifts"]//div[@class="title"]//a')
+
+    def _load_settings(self):
+        return json.load(open(SETTINGS_PATH))
+
+    def _save_settings(self):
+        json.dump(self.settings, open(SETTINGS_PATH, "w"))
+
+    def scan(self):
+        games = self.settings["games"]
+        new_games = {g.text for g in self._scan()}
+        for ng in new_games:
+            if ng not in games:
+                decision = raw_input("New game found: '" + ng +
+                                     "'. Do you want to automatically access its giveaways (y/n)?")
+                if decision == 'y':
+                    self.settings["games"][ng] = 1
+                else:
+                    self.settings["games"][ng] = 0
+        self._save_settings()
 
     def estimo(self):
-        driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_css_selector("img.login").click()
-        driver.find_element_by_id("steamAccountName").clear()
-        driver.find_element_by_id("steamAccountName").send_keys(self.username)
-        driver.find_element_by_id("steamPassword").clear()
-        driver.find_element_by_id("steamPassword").send_keys(self.password)
-        driver.find_element_by_id("imageLogin").click()
-        games = driver.find_elements_by_xpath('//div[@class="ajax_gifts"]//div[@class="title"]//a')
+        self.login()
+        games = self._scan()
         for game in games:
                 print(game.text)
         # driver.find_element_by_link_text("Assassin's Creed: Director's Cut Edition").click()
@@ -41,4 +68,7 @@ class Estimo:
 
 
 if __name__ == "__main__":
-    Estimo().run()
+    estimo = Estimo()
+    if len(sys.argv) == 2 and sys.argv[1] == '-scan':
+        estimo.scan()
+        estimo.close()
