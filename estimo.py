a2a1128d793dc8e26024cea7a2941ed9b2783567
dyeray <dyeray@ull.es>
2014-10-13 14:29:28 +0100

    Hide browser window unless debug option is used

--- estimo.py
+++ estimo.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from pyvirtualdisplay import Display
 import json
 import time
 import argparse
@@ -11,19 +12,25 @@ SETTINGS_PATH = 'settings.cfg'
 
 class Estimo:
     def __init__(self):
-        self.drv = webdriver.Firefox()
-        self.drv.implicitly_wait(10)
         self.base_url = "http://www.steamgifts.com"
         self.page_url = "http://www.steamgifts.com/open/page/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
         self.settings = self._load_settings()
+
+    def _stop_driver(self):
+        self.drv.quit()
+        self.display.stop()
+
+    def _start_driver(self, dbg=False):
+        self.display = None
+        if not dbg:
+            display = Display(visible=0, size=(1280, 1014))
+            display.start()
+            self.display = display
+        self.drv = webdriver.Firefox()
+        self.drv.implicitly_wait(10)
         self.base_window = self.drv.current_window_handle
 
-    def close(self):
-        self.drv.quit()
-
-    def login(self):
+    def _login(self):
         self.drv.get(self.base_url + "/")
         self.drv.find_element_by_css_selector("img.login").click()
         self.drv.find_element_by_id("steamAccountName").clear()
@@ -50,7 +57,8 @@ class Estimo:
     def _save_settings(self):
         json.dump(self.settings, open(SETTINGS_PATH, "w"))
 
-    def scan(self, deep=False):
+    def scan(self, deep=False, dbg=False):
+        self._start_driver(dbg)
         games = self.settings["games"]
         new_games = {g.text for g in self._scan()}
         for ng in new_games:
@@ -64,9 +72,11 @@ class Estimo:
                 else:
                     break
         self._save_settings()
+        self._stop_driver()
 
-    def subscribe(self, deep=False):
-        self.login()
+    def subscribe(self, deep=False, debug=False):
+        self._start_driver(debug)
+        self._login()
         wanted_games = {k for k, v in self.settings["games"].items() if v == 1}
         games = self._scan()
         for game in games:
@@ -80,6 +90,7 @@ class Estimo:
                     pass
                 self.drv.close()
                 self.drv.switch_to_window(self.base_window)
+        self._stop_driver()
 
 
 if __name__ == "__main__":
@@ -91,9 +102,8 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--debug', const=True, default=False, nargs='?',
                         help='Show the browser window')
     estimo = Estimo()
-    args = parser.parse_args()
-    if args.operation == 'scan':
-        estimo.scan(args.deep)
+    argss = parser.parse_args()
+    if argss.operation == 'scan':
+        estimo.scan(argss.full, argss.debug)
     else:
-        estimo.subscribe(args.deep)
-    estimo.close()
+        estimo.subscribe(argss.full, argss.debug)
